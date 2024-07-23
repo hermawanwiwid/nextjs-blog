099a190666d400d592f59ab50ba52534dd981880
hermawanwiwid <hermawanwiwid@gmail.com>
2024-07-23 15:31:43 +0700

    Implement getStaticProps

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,5 +1,5 @@
 import Layout from "../../components/layout";
-import { getAllPostIds } from "../../lib/posts";
+import { getAllPostIds, getPostData } from "../../lib/posts";
 
 export default function Post() {
   return <Layout>...</Layout>;
@@ -14,5 +14,10 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-  // Fetch necessary data for the blog post using params.id
+  const postData = getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
 }
