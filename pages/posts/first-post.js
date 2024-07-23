bc9fdb8302e38f1268721e6b520d332334f39178
hermawanwiwid <hermawanwiwid@gmail.com>
2024-07-23 14:13:12 +0700

    add Head and Script

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,14 +1,26 @@
 import React from "react";
 import Link from "next/link";
+import Head from "next/head";
+import Script from "next/script";
 
 const index = () => {
   return (
-    <div>
+    <>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={() =>
+          console.log(`script loaded correctly, window.FB has been populated`)
+        }
+      />
       <h1>First Post</h1>
       <h2>
-        <Link href="/">Back to Home</Link>
+        <Link href="/">← Back to Home</Link>
       </h2>
-    </div>
+    </>
   );
 };
 
